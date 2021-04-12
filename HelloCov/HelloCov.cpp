fd8e363292f3f5145d2195d749e1b96f43456c1c
Imaculate <imaculatemosha@yahoo.com>
2021-04-12 12:21:03 +0400

    Passing tests

--- HelloCov/HelloCov.cpp
+++ HelloCov/HelloCov.cpp
@@ -1,10 +1,19 @@
-// HelloCov.cpp : Defines the functions for the static library.
-//
+#include "HelloCov.h"
 
-#include "pch.h"
-#include "framework.h"
-
-// TODO: This is an example of a library function
-void fnHelloCov()
+bool is_power_of_two(int n)
 {
+    if (n < 0)
+        return false;
+
+    return (n & (n - 1)) == 0;
+}
+
+int multiply_by_two(int n)
+{
+    return n << 1;
+}
+
+int divide_by_two(int n)
+{
+    return n >> 1;
 }
---
+++ HelloTest/HelloTest.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../HelloCov/HelloCov.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HelloCovTest
+{
+	TEST_CLASS(Test1)
+	{
+	public:
+
+		TEST_METHOD(IsPowerOfTwo)
+		{
+			Assert::IsTrue(is_power_of_two(8));
+		}
+
+		TEST_METHOD(MultiplyByTwo)
+		{
+			Assert::AreEqual(14, multiply_by_two(7));
+		}
+
+		TEST_METHOD(DivideByTwo)
+		{
+			Assert::AreEqual(-4, divide_by_two(-7));
+		}
+	};
+}
